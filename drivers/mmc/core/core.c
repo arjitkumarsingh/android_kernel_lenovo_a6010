a2f6d480e1797a0688c073d933655eb98b1fcbaf
Jaroslav Zviezda <acroreiser@gmail.com>
2019-12-30 10:38:50 +0300

    Revert "disable MMC SPI CRC"
    This reverts commit 1fce5f4e93560211a3380dc475aaeaccc5d63a0b.

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -68,10 +68,8 @@ static const unsigned freqs[] = { 400000, 300000, 200000, 100000 };
  * performance cost, and for other reasons may not always be desired.
  * So we allow it it to be disabled.
  */
-int use_spi_crc = 0;
-EXPORT_SYMBOL(use_spi_crc);
-module_param_named(crc, use_spi_crc, int, 0644);
-MODULE_PARM_DESC(crc, "Enable/disable CRC");
+bool use_spi_crc = 0;
+module_param(use_spi_crc, bool, 0);
 
 /*
  * We normally treat cards as removed during suspend if they are not
